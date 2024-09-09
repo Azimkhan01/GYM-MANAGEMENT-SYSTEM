4dff70f37f14926092409b9be4ead1d31861b75c
Azimkhan01 <azimkarimrahim@gmail.com>
2024-09-09 20:49:48 +0530

    Added searching dynamically with whatsapp number , name

--- public/js/remove.js
+++ public/js/remove.js
@@ -7,9 +7,9 @@ searchInput.addEventListener("input", (e) => {
         // console.log(e.target.value)
         if (r[i]["id"] == e.target.value) {
           let resultSection = document.getElementById("resultSection");
-          let deleteButton= document.getElementById("deleteButton")
+          let deleteButton = document.getElementById("deleteButton");
           deleteButton.disabled = false;
-          deleteButton.style.backgroundColor = "#444444"
+          deleteButton.style.backgroundColor = "#444444";
           resultSection.innerHTML = `
       <div id="membershipDetails" class="membershipDetails">
       <img src="${r[i]["image"]}"  alt="${r[i]["name"]}" class="passport-image">
@@ -27,10 +27,52 @@ searchInput.addEventListener("input", (e) => {
             '<p id="resultText">Search result will appear here...</p>';
           let resultText = document.getElementById("resultText");
           resultText.textContent = `${e.target.value} not found`;
-          let deleteButton= document.getElementById("deleteButton")
+          let deleteButton = document.getElementById("deleteButton");
           deleteButton.disabled = true;
-          deleteButton.style.backgroundColor = "tomato"
+          deleteButton.style.backgroundColor = "tomato";
         }
       }
+    });
+});
+
+let memberSearchInput = document.getElementById("memberSearchInput");
+
+memberSearchInput.addEventListener("input", (e) => {
+  const searchTerm = e.target.value.toLowerCase();
+
+  fetch("http://127.0.0.1:8000/memberApi")
+    .then((response) => response.json())
+    .then((data) => {
+      // Filter results based on the search term
+      const results = data.filter(
+        (item) =>
+          item["name"].toLowerCase().includes(searchTerm) ||
+          item["whatsapp"].toLowerCase().includes(searchTerm)
+      );
+
+      if (results.length != data.length) {
+        let searchTable = document.getElementById("searchTable");
+        searchTable.innerHTML = "";
+        results.filter((value) => {
+          let tr = document.createElement("tr");
+          tr.id = value["id"];
+          tr.innerHTML = `<td>jp-1</td>
+                            <td>${value["name"]}</td>
+                            <td>${value["whatsapp"]}</td>
+                            <td>${value["membership_date"]}</td>
+                            <td>${value["membership_duration"]}</td>
+                            <td>${value["expiry"]}</td>
+                            <td>${value["fees_paid"]}</td>
+                            <td>${value["offer"]}</td>`;
+
+          searchTable.appendChild(tr);
+        });
+      } else {
+        let searchTable = document.getElementById("searchTable");
+        searchTable.innerHTML = "";
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
 });
