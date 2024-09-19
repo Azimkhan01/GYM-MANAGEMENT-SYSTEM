e35b9a9931194e9a3c0d8172bfc3086de178d8b6
EverAzim <113935317+Azimkhan01@users.noreply.github.com>
2024-09-19 18:21:47 +0530

    Update handleUpdate.js. The error is the update is not updating the expiry again suppose if the person changed the expiry of the person then you can see that it is chaged

--- controller/handleUpdate.js
+++ controller/handleUpdate.js
@@ -1,31 +1,83 @@
-const {membership} = require("../database/registeredUser");
-require("dotenv").config()
-const handleUpdate =async (req,res)=>{
-    let name = await req.body.name
-    let gmail = await req.body.gmail
-    let membership_date = await req.body.membership_date;
-    let membership_duration = await req.body.membership_duration;
-    let fees_paid = await req.body.fees_paid;
-    let whatsapp = await req.body.whatsapp;
-    let offer = await req.body.offer;
-    let image = await req.body.image;
-    // console.log(req.body)
-let data =await  membership.updateOne({whatsapp:whatsapp},{name:name,gmail:gmail,membership_date:membership_date,membership_duration:membership_duration,fees_paid:fees_paid,offer:offer,image:image});
-// console.log(data)  
-if(data)
-  {
-    res.render("update",{
-        gymName:process.env.gymName,
-        color:'green',
-        status:`Update of ${name} is commited succesfully`
-    })
-  }else{
-    res.render("update",{
-      gymName:process.env.gymName,
-        status:`update not happened `
-    })
-  }
-    
+const { membership } = require("../database/registeredUser");
+require("dotenv").config();
+
+function getExpiry(startDate, duration) {
+    const start = new Date(startDate);
+
+    switch (duration) {
+        case '1-month':
+            start.setMonth(start.getMonth() + 1);
+            break;
+        case '3-months':
+            start.setMonth(start.getMonth() + 3);
+            break;
+        case '6-months':
+            start.setMonth(start.getMonth() + 6);
+            break;
+        case '1-year':
+            start.setFullYear(start.getFullYear() + 1);
+            break;
+        default:
+            return null;
+    }
+
+    const expiryDate = start.toISOString().split('T')[0];
+    return expiryDate;
 }
 
-module.exports = {handleUpdate};
+const handleUpdate = async (req, res) => {
+  try {
+    let name = req.body.name;
+    let gmail = req.body.gmail;
+    let membership_date = req.body.membership_date;
+    let membership_duration = req.body.membership_duration;
+    let fees_paid = req.body.fees_paid;
+    let whatsapp = req.body.whatsapp;
+    let offer = req.body.offer;
+    let image = req.body.image;
+
+    let expiry_date = getExpiry(membership_date, membership_duration);
+
+    if (!expiry_date) {
+      res.render("update", {
+        gymName: process.env.gymName,
+        status: `Invalid membership duration provided for ${name}`
+      });
+      return;
+    }
+
+    let data = await membership.updateOne(
+      { whatsapp: whatsapp },
+      {
+        name: name,
+        gmail: gmail,
+        membership_date: membership_date,
+        membership_duration: membership_duration,
+        fees_paid: fees_paid,
+        offer: offer,
+        image: image,
+        expiry_date: expiry_date
+      }
+    );
+
+    if (data.nModified > 0) {
+      res.render("update", {
+        gymName: process.env.gymName,
+        color: 'green',
+        status: `Update of ${name} was committed successfully`
+      });
+    } else {
+      res.render("update", {
+        gymName: process.env.gymName,
+        status: `No changes were made to ${name}'s data`
+      });
+    }
+  } catch (error) {
+    res.render("update", {
+      gymName: process.env.gymName,
+      status: `An error occurred during the update`
+    });
+  }
+};
+
+module.exports = { handleUpdate };
